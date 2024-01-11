24bd376250c264abe8ce650a69e4452182191d06
yassinechidm <yassinechidmi2002@icloud.com>
2024-01-11 14:16:36 +0100

    Initial commit

---
+++ src/main/java/com/Yassine_amine/App.java
@@ -0,0 +1,13 @@
+package com.Yassine_amine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
